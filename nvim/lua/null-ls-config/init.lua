89145327845d69795555f7b1787f13298c4cd8a5
marcoswlrich <marcosapw.dev@gmail.com>
2022-05-01 19:32:58 -0300

    Config errors Null-ls

--- nvim/lua/null-ls-config/init.lua
+++ nvim/lua/null-ls-config/init.lua
@@ -11,18 +11,14 @@ null_ls.setup({
         '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
         '--break-after-table-lb', '--indent-width=2'
       }
-    }), formatting.isort, formatting.codespell.with({ filetypes = { 'markdown' } })
+    }), formatting.isort, formatting.codespell.with({ filetypes = { 'markdown' } }),
+    null_ls.builtins.code_actions.gitsigns.with({
+        disabled_filetypes = { "lua" },
+    })
   },
   on_attach = function(client)
       if client.resolved_capabilities.document_formatting then
           vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
       end
-      vim.cmd [[
-      augroup document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]]
-  end
+  end,
 })
